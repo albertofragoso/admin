1b19af340a53fff2251dd441d592e10dacf8425e
albertofragoso <hola@albertofragoso.com>
2019-08-26 17:12:50 -0500

    Creating Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import logo from '../images/logo.png'
+
+import './styles/Sidebar.css'
+
+const Sidebar = () => {
+
+  const [isToggle, setToogle] = useState(false)
+
+  const SidebarToggle = classNames('Sidebar', { toggle: isToggle })
+
+  const handleToggle = () => setToogle(!isToggle)
+
+  return(
+    <div className="bg-light border-right">
+    <span className="Sidebar-button" onClick={handleToggle}>🍔</span>
+    <div className={SidebarToggle}>
+      <div className="Sidebar-logo">
+        <img src={logo} alt="Admin logo" />
+        <span>Conektando</span>
+      </div>
+      <div className="list-group list-group-flush">
+        <a href="#" className="list-group-item list-group-item-action bg-light">💳 Payments</a>
+        <a href="#" className="list-group-item list-group-item-action bg-light">📤 Transfers</a>
+        <a href="#" className="list-group-item list-group-item-action bg-light">🔄 Chargebacks</a>
+        <a href="#" className="list-group-item list-group-item-action bg-light">🧮 Accounting</a>
+        <a href="#" className="list-group-item list-group-item-action bg-light">💻 Developers</a>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Sidebar
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 const App = () => (
-  <>
+  <div className="d-flex">
+    <Sidebar />    
     <h1>Hello World!</h1>
-  </>
+  </div>
 )
 
 export default App
