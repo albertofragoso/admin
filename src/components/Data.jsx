cefe08dfe45cc70a4d7ecd9f65a98fa478d18d7f
albertofragoso <hola@albertofragoso.com>
2019-08-27 20:52:05 -0500

    Finishing PaymentDetail page

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -4,14 +4,14 @@ import Client from '../components/Client'
 import PaymentMethod from '../components/PaymentMethod'
 
 const Data = ({ payment }) => (
-  <div class="row">
-    <div class="col-md-6 col-sm-12 mb-3">
+  <div className="row">
+    <div className="col-md-6 col-sm-12 mb-3">
       <PaymentStatus payment={payment} />
     </div>
-    <div class="col-md-3 col-sm-12 mb-3">
+    <div className="col-md-3 col-sm-12 mb-3">
       <Client payment={payment} />
     </div>
-    <div class="col-md-3 col-sm-12 mb-3">
+    <div className="col-md-3 col-sm-12 mb-3">
      <PaymentMethod payment={payment} />
     </div>  
   </div>
--- src/pages/PaymentDetail.jsx
+++ src/pages/PaymentDetail.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Data from '../components/Data'
 import Breakdown from '../components/Breakdown'
+
+import './styles/PaymentDetail.css'
 
 const PaymentDetail = props => {
 
   const payment = props.location.state
 
+  const [data, setData] = useState(true)
+
+  const handleClick = () => setData(!data)
+
   return(
-    <div className="container-fluid">
+    <div className="PaymentDetail container-fluid">
       <h1 className="mt-4">Payment Details</h1>
-
-      {/* <Data payment={payment} /> */}
-
-      <Breakdown payment={payment} />
-
+      <ul className="nav nav-tabs mb-3">
+        <li className="nav-item">
+          <span onClick={handleClick} className={data ? `nav-link active` : 'nav-link'}>Data</span>
+        </li>
+        <li className="nav-item">
+          <span onClick={handleClick} className={!data ? `nav-link active` : 'nav-link'}>Breakdown</span>
+        </li>
+      </ul>
+      {data
+       ? <Data payment={payment} />
+       : <Breakdown payment={payment} />
+      }
     </div>
-
   )
 }
 
